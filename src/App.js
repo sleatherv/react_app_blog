8168e26b445718471dbfae03b6cfcbe24775454e
Sleather Vega <sleatherv28@hotmail.com>
2021-04-13 21:18:13 -0500

    Create new components

--- src/App.js
+++ src/App.js
@@ -1,45 +1,36 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { BrowserRouter, Route } from 'react-router-dom';
+import About from './components/About';
+import Blog from './components/Blog';
+import Header from './components/Header';
+import Home from './components/Home';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <div>
-        <header>
-          <h1>My personal blog</h1>
-          <nav>
-            <Link to="/">Home</Link>
-            <Link to="/blog">Blog</Link>
-            <Link to="/about">About</Link>
-          </nav>
-        </header>
-        <main>
-          <Route path="/" exact={true}>
-            <div>
-              <h2>Home page</h2>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-            </div>
-          </Route>
-          <Route path="/blog">
-            <div>
-              <h2>Blog</h2>
-              <ul>
-                <li>Article 1</li>
-                <li>Article 2</li>
-                <li>Article 3</li>
-              </ul>
-            </div>
-          </Route>
-          <Route path='/about'>
-            <div>
-              <h2>About</h2>
-              <ul>
-                <p>Hi, I'm Sleather</p>
-              </ul>
-            </div>
-          </Route>
-        </main>
-      </div>
+      <MainContainer>
+        <Header />
+        <Main>
+          <Route path="/" exact={true} component={Home} />
+          <Route path="/blog" component={Blog} />
+          <Route path='/about' component={About} />
+
+        </Main>
+      </MainContainer>
     </BrowserRouter>
   )
 }
+
+const MainContainer = styled.div`
+    padding: 40px;
+    width: 90%;
+    max-width: 700px;
+`;
+
+const Main = styled.main`
+  background:#fff;
+  padding: 40px;
+  border-radius:10px;
+`;
+
